f000b4aff9a45d97e32721d88b73e640326aead6
Vlad Smolyaninov <machine@Vlads-MacBook-Pro-8.local>
2019-10-21 21:54:20 +0300

    add global styles

---
+++ src/services/styles/global.ts
@@ -0,0 +1,11 @@
+ // @ts-ignore
+import { setCustomText } from 'react-native-global-props'
+
+const customTextProps = {
+	style: {
+		fontFamily: 'Open sans',
+		color: 'black'
+	}
+}
+
+setCustomText(customTextProps)
